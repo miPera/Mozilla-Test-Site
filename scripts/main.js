105976d6e7fb367e3eef635d3a53bd10071b58b3
miPera <migperalta10@gmail.com>
2018-02-27 21:25:42 -0800

    User enters name when first entering site and site greets that user

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,36 @@
+//get reference to image on index page
 var myImage = document.querySelector('img');
 
+//function that changes image when image is clicked
 myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox-icon2.png');
-    } else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
-    }
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/firefox-icon.png') {
+		myImage.setAttribute ('src','images/firefox-icon2.png');
+	} else {
+		myImage.setAttribute ('src','images/firefox-icon.png');
+	}
+}
+
+//get references to button and header
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+//function that sets username
+function setUserName() {
+	var myName = prompt('Please enter your name.');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+//Set localstorage username
+if(!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+//create on-click event handler
+myButton.onclick = function() {
+  setUserName();
 }
